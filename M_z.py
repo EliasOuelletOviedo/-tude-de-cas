c932fd3568efeaa95b67b67b626a870d8d97c6a4
NathanBerube <naber260@ulaval.ca>
2025-04-15 23:55:54 -0400

    a

--- M_z.py
+++ M_z.py
@@ -3,25 +3,24 @@ from graphique_effort_interne import graph
 import matplotlib.pyplot as plt
 from scipy.integrate import cumtrapz
 
+
 plt.rcParams['text.usetex'] = True
 
-
 n_points_zone = 100
+end_zone1 = 0.75
+end_zone2 = 0.95
 length = 0.95
 
-v_z_zone1 = 1263
-v_z_zone2 = -737
+v_y_zone1 = 632 
+v_y_zone2 = -2368 
 
-end_zone1 = 0.35
-end_zone2 = 0.95
-
-v_z = [0,v_z_zone1, v_z_zone1, v_z_zone2, v_z_zone2, v_z_zone2, 0]
+v_y = [0,v_y_zone1, v_y_zone1, v_y_zone2, v_y_zone2, v_y_zone2, 0]
 x =   [0,0, end_zone1, end_zone1, end_zone2, end_zone2, length]
 
-M_z = cumtrapz(v_z, x, initial=0)
+M_y = -1 * cumtrapz(v_y, x, initial=0)
 
 x_upsampled = np.linspace(x[0], x[-1], 200)
-M_z_upsampled = np.interp(x_upsampled, x, M_z)
-
-graph(x_upsampled, M_z_upsampled, r'Position $x$ [m]', r'Moment de flexion $M_z(x)$ [N$\cdot$m]')
+M_y_upsampled = np.interp(x_upsampled, x, M_y)
+print(max(np.abs(M_y_upsampled)))
+graph(x_upsampled, M_y_upsampled, r'Position $x$ [m]', r'Moment de flexion $M_z(x)$ [N$\cdot$m]')
 plt.show()
--- V_y.py
+++ V_y.py
@@ -16,5 +16,6 @@ v_y_zone2 = -2368
 v_y = [0,v_y_zone1, v_y_zone1, v_y_zone2, v_y_zone2, v_y_zone2, 0]
 x =   [0,0, end_zone1, end_zone1, end_zone2, end_zone2, length]
 
+print(max(np.abs(v_y)))
 graph(x, v_y, r'Position $x$ [m]', r'Effort tranchant $V_y(x)$ [N]')
 plt.show()
